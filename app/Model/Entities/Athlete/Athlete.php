a158a4521e78c5cd7e9e561729176a29b100e4ec
lenka <loksik.lenka@gmail.com>
2022-11-11 09:22:09 +0100

    created Discipline created Athlete

---
+++ app/Model/Entities/Athlete/Athlete.php
@@ -0,0 +1,150 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Entities\Athlete;
+
+use App\Model\Entities\Gender\Gender;
+use App\Model\Entities\Sport\Sport;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="athlete")
+ */
+class Athlete
+{
+    /**
+     * @var int
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    private int $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private string $firstname;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private string $lastname;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Model\Entities\Gender\Gender")
+     * @ORM\JoinColumn(name="gender_id", referencedColumnName="id", nullable=false)
+     */
+    private Gender $gender;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private string $country;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Model\Entities\Sport\Sport")
+     * @ORM\JoinColumn(name="sport_id", referencedColumnName="id", nullable=false)
+     */
+    private Sport $sport;
+
+    public function __construct(string $firstname, string $lastname, Gender $gender, string $country, Sport $sport)
+    {
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->gender = $gender;
+        $this->country = $country;
+        $this->sport = $sport;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param string $firstname
+     */
+    public function setFirstname(string $firstname): void
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param string $lastname
+     */
+    public function setLastname(string $lastname): void
+    {
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * @return Gender
+     */
+    public function getGender(): Gender
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param Gender $gender
+     */
+    public function setGender(Gender $gender): void
+    {
+        $this->gender = $gender;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param string $country
+     */
+    public function setCountry(string $country): void
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * @return Sport
+     */
+    public function getSport(): Sport
+    {
+        return $this->sport;
+    }
+
+    /**
+     * @param Sport $sport
+     */
+    public function setSport(Sport $sport): void
+    {
+        $this->sport = $sport;
+    }
+}
---
+++ app/Model/Entities/Discipline/Discipline.php
@@ -0,0 +1,104 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Entities\Discipline;
+
+use App\Model\Entities\Gender\Gender;
+use App\Model\Entities\Sport\Sport;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="discipline")
+ */
+class Discipline
+{
+    /**
+     * @var int
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    private int $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Model\Entities\Sport\Sport")
+     * @ORM\JoinColumn(name="sport_id", referencedColumnName="id", nullable=false)
+     */
+    private Sport $sport;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Model\Entities\Gender\Gender")
+     * @ORM\JoinColumn(name="gender_id", referencedColumnName="id", nullable=false)
+     */
+    private Gender $gender;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private string $name;
+
+    public function __construct(Sport $sport, Gender $gender, string $name)
+    {
+        $this->sport = $sport;
+        $this->gender = $gender;
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Sport
+     */
+    public function getSport(): Sport
+    {
+        return $this->sport;
+    }
+
+    /**
+     * @param Sport $sport
+     */
+    public function setSport(Sport $sport): void
+    {
+        $this->sport = $sport;
+    }
+
+    /**
+     * @return Gender
+     */
+    public function getGender(): Gender
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param Gender $gender
+     */
+    public function setGender(Gender $gender): void
+    {
+        $this->gender = $gender;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
